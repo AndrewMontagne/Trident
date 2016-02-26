29c49189b787bde0561a7a0346f3303852533abe
Andrew Montagne <andrewmontagne@gmail.com>
2016-02-26 22:14:09 +0000

    Added some libraries, worked on GitCgi.

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <?php
 require 'vendor/autoload.php';
 
+
 if(preg_match('/^git\/\d+\.\d+\.\d+$/', $_SERVER['HTTP_USER_AGENT'])) { //Git client
-    Flight::route('/git/@repo/', function($repo) {
-        $git = new \Trident\GitCgi($repo);
-        $git->handle();
-    });
+    Flight::route('/git/*', function($route) {
+        $git = new \Trident\GitCgi($route->splat);
+        $git->handleCgi();
+    }, true);
 } else { //Web browser
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
     Flight::route('/', function() {
-        require($_SERVER['DOCUMENT_ROOT'] . '/src/views/index.phtml');
+        require(ROOT_DIR . '/src/views/index.phtml');
     });
 }
+
+define('ROOT_DIR', $_SERVER['DOCUMENT_ROOT']);
+
 Flight::start();
--- src/Trident/GitCgi.php
+++ src/Trident/GitCgi.php
@@ -8,9 +8,14 @@
 
 namespace Trident;
 
+//Refer to: http://www.tcl.tk/man/aolserver3.0/cgi-ch4.htm
 
 class GitCgi
 {
+    const PIPE_STDIN = 0;
+    const PIPE_STDOUT = 1;
+    const PIPE_STDERR = 2;
+
     /**
      * @var string
      */
@@ -22,12 +27,107 @@ class GitCgi
      */
     public function __construct($repo)
     {
-        $this->repoName = $repo;
+        $this->repoName = '/' . $repo;
     }
 
-    public function handle()
+    /**
+     * Handles the invocation of the Git CGI interface
+     *
+     * Adapted from http://nerds-central.blogspot.co.uk/2007/08/php-cgi-handler-using-procopen.html
+     * Many thanks to Alexander Turner
+     */
+    public function handleCgi()
     {
-        echo 'This will eventually execute <pre>git http-backend</pre>';
+        $method = $_SERVER['REQUEST_METHOD'];
+        $pipes = [];
+        $environmentVariables = [
+            'GIT_PROJECT_ROOT' => '/media/sf_Development',
+            'PATH_INFO' => $this->repoName,
+            'REMOTE_USER' => 'andrew',
+            'REMOTE_ADDR' => 'git.local',
+            'QUERY_STRING' => $_SERVER['QUERY_STRING'],
+            'GIT_HTTP_EXPORT_ALL' => 'TRUE',
+            'REQUEST_METHOD' => $method,
+        ];
+        $otherOptions = [
+            'suppress_errors' => true,
+            'bypass_shell' => true,
+        ];
+
+        $descriptorSpec = [];
+        if($method == 'POST') {
+            $descriptorSpec[self::PIPE_STDIN] = ['pipe', 'r'];
+            $descriptorSpec[self::PIPE_STDOUT] = ['pipe', 'w'];
+            $descriptorSpec[self::PIPE_STDERR] = ['pipe', 'w'];
+        } else {
+            $descriptorSpec[self::PIPE_STDOUT] = ['pipe', 'w'];
+        }
+
+        $git = proc_open(
+            'git http-backend',
+            $descriptorSpec,
+            $pipes,
+            ROOT_DIR,
+            $environmentVariables,
+            $otherOptions);
+
+        if($git && $method == 'POST') {
+            $readbuffer = fopen('php://input', 'r');
+            $toWrite = '';
+            while (true) {
+                if ($toWrite == '') {
+                    if (feof($readbuffer)) break;
+                    $toWrite = fread($readbuffer, 4096);
+                }
+                $l = fwrite($pipes[self::PIPE_STDIN], $toWrite);
+                if ($l === false) {
+                    echo 'Error writing to process';
+                } else {
+                    if ($l == 0) {
+                        echo "Empty </br>";
+                        usleep(1000);
+                    } else {
+                        $toWrite = '';
+                    }
+                }
+                fflush($pipes[self::PIPE_STDIN]);
+            }
+        } else if(!$git) {
+            header("HTTP/1.0 500 Internal Server Error");
+            echo "<h1>Could not open cgi process</h1>";
+            exit(1);
+        }
+
+        $cgi_out = $pipes[self::PIPE_STDOUT];
+
+        $totalResponse = '';
+
+        $buffer = '';
+        while(!feof($cgi_out)) {
+            $char = fread($cgi_out, 1);
+            $totalResponse .= $char;
+
+            if("\n" == $char) {
+                if(strlen($buffer) < 2) {
+                    break;
+                }
+                header($buffer);
+                $buffer = '';
+            } else {
+                $buffer .= $char;
+            }
+
+            $lastChar = $char;
+        }
+
+        while(!feof($cgi_out)) {
+            $data = fread($cgi_out, 8192);
+            echo $data;
+            $totalResponse .= $data;
+        }
+
+        error_log($totalResponse);
+
         exit(0);
     }
 }
