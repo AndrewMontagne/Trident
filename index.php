3c9c4c2b0c5182061cc5d2bfb9904688dcef5829
Andrew Montagne <andrewmontagne@gmail.com>
2016-03-15 22:54:47 +0000

    Case Sensitivity

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if(preg_match('/^git\/.*$/', $_SERVER['HTTP_USER_AGENT'])) { //Git client
         echo '{"size":1,"limit":25,"isLastPage":true,"values":[{"slug":"repo","id":2,"name":"repo","scmId":"git","state":"AVAILABLE","statusMessage":"Available","forkable":true,"project":{"key":"~ANDREW","id":2,"name":"andrew","type":"PERSONAL","owner":{"name":"andrew","emailAddress":"andrewmontagne@gmail.com","id":1,"displayName":"andrew","active":true,"slug":"andrew","type":"NORMAL","links":{"self":[{"href":"http://localhost:7990/users/andrew"}]}},"links":{"self":[{"href":"http://localhost:7990/users/andrew"}]}},"public":false,"links":{"clone":[{"href":"http://andrew@localhost:7990/scm/~andrew/repo.git","name":"http"}],"self":[{"href":"http://localhost:7990/users/andrew/repos/repo/browse"}]}}],"start":0}';
     });
     Flight::route('/', function() {
-        require(ROOT_DIR . '/src/views/index.phtml');
+        require(ROOT_DIR . '/src/Views/index.phtml');
     });
 }
 
